283f7a7996a9648a8a08ae14b6487f0b0f36b338
Vivian Li <vivian137r.22@gmail.com>
2025-03-29 19:08:16 -0700

    added pixelated fonts and custom colors

--- hobby-app/src/components/MatchConfirm.tsx
+++ hobby-app/src/components/MatchConfirm.tsx
@@ -5,24 +5,28 @@ type MatchConfirmProps = {
   
   const MatchConfirm = ({ onConfirm, onCancel }: MatchConfirmProps) => {
     return (
-      <div className="px-6 py-3 w-[1130px] flex items-center justify-between border-4 border-black bg-white ">
-        {/* Text */}
-        <span className="text-black font-semibold text-black">Enter Match?</span>
+        <div className="px-10 py-6 w-[1110px] h-[120px] flex items-center justify-between border-8 border-black bg-game-white shadow-[8px_8px_0_#333333]">
+        {/* Left Text */}
+        <span className="font-jersey text-black text-[80px] px-6 font-semibold">
+          Enter Match?
+        </span>
   
+        {/* Right-aligned Button Group */}
+        <div className="flex gap-6">
           <button
             onClick={onConfirm}
-            className="font-pixel bg-game-green text-white px-16 py-2 border-4 border-black hover:brightness-105 shadow-pixel"
+            className="font-jersey bg-game-green text-white text-[55px] flex items-center justify-center w-[220px] h-[80px] border-[6px] border-black hover:brightness-105 shadow-[6px_6px_0_#333333]"
           >
             Yes
           </button>
-
+  
           <button
             onClick={onCancel}
-            className="font-pixel bg-game-red text-white px-16 py-2 border-4 border-black hover:brightness-105 shadow-pixel"
+            className="font-jersey bg-game-red text-white text-[55px] flex items-center justify-center w-[220px] h-[80px] border-[6px] border-black hover:brightness-105 shadow-[6px_6px_0_#333333]"
           >
             No
           </button>
-
+        </div>
       </div>
     );
   };
--- hobby-app/src/pages/Discover.tsx
+++ hobby-app/src/pages/Discover.tsx
@@ -47,46 +47,10 @@ const DiscoverPage = () => {
         }}
       />
 
-      {/* Opponent Info Speech Bubble */}
-      <div className="absolute top-4 left-4 bg-[#e0f6f7] border-4 border-black px-4 py-3 shadow-[4px_4px_0_#000]">
-        <div className="flex items-center gap-2">
-          <h2 className="text-xl font-bold">{opponent.name},</h2>
-          <span className="text-lg font-bold">{opponent.age}</span>
-          <span className="text-sm">{opponent.location}</span>
-        </div>
-        <div className="mt-2 flex flex-wrap gap-2">
-          {opponent.hobbies.map((hobby) => (
-            <span
-              key={hobby}
-              className="px-2 py-1 text-sm font-semibold text-white border-2 border-black shadow bg-gray-600"
-            >
-              {hobby}
-            </span>
-          ))}
-        </div>
-      </div>
 
-      {/* Opponent Avatar */}
-      <img
-        src={opponent.avatar}
-        alt={opponent.name}
-        className="absolute top-36 right-20 w-16 h-16"
-      />
-
-      {/* User Avatar */}
-      <img
-        src="https://archives.bulbagarden.net/media/upload/9/9a/Spr_RS_May.png"
-        alt="You"
-        className="absolute bottom-32 left-20 w-20 h-20"
-      />
-
-      {/* User Name Bubble */}
-      <div className="absolute bottom-44 right-16 bg-[#e0f6f7] border-4 border-black px-4 py-2 shadow-[4px_4px_0_#000]">
-        <span className="font-bold text-lg">Bob, 24</span>
-      </div>
 
       {/* Match Confirmation Box */}
-      <div className="absolute bottom-16 left-1/2 -translate-x-1/2 px-6 py-3">
+      <div className="absolute bottom-16 left-1/2 -translate-x-1/2 px-6 py-1">
         <MatchConfirm onConfirm={handleConfirm} onCancel={handleCancel} />
       </div>
 
